46267b78cf90058e251db717ec987e06950711e7
Justine Manalili Pham <tinemanalili@gmail.com>
2024-11-06 12:42:47 +0100

    Updates

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import { HashRouter as Router } from 'react-router-dom'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <Router>
-  <App />
-</Router>,
-)
